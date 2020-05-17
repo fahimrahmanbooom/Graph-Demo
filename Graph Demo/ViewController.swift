d615cef279dc474cd814bd5ac8ef1dc8b61f9b68
circuit <fahimrahmanbooom@gmail.com>
2020-05-17 11:24:46 +0600

    initial

--- Graph Demo/ViewController.swift
+++ Graph Demo/ViewController.swift
@@ -7,14 +7,52 @@
 //
 
 import UIKit
+import SwiftChart
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var graphView: Chart!
+    
+    let data: [Double] = [35.92, 34.3, 31.76, 29.23, 27.58, 26.43]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //let labelsAsString = ["" ,"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", ""]
+        
+        let labelsAsString = ["1" ,"2", "3", "4", "5", "6"]
+        
+        let series = ChartSeries(data)
+        series.colors = (
+            above: ChartColors.purpleColor(),
+            below: ChartColors.orangeColor(),
+            zeroLevel: 26.43
+        )
+        series.area = true
+        
+        graphView.add(series)
+        print("###",data)
+        // Set minimum and maximum values for y-axis
+        graphView.minY = 26.43
+        graphView.maxY = 35.92
+
+        // Format y-axis, e.g. with units
+        graphView.yLabelsFormatter = { String(Int($1)) + " ºC" }
+        
+        
+        graphView.xLabelsFormatter = { (labelIndex: Int, labelValue: Double) -> String in
+
+            return labelsAsString[labelIndex]
+        }
+
     }
-
-
+    
+//    func showControllerWithLabel() {
+//
+//        label.countFrom(23.01 - 1, to: 23.01, withDuration: 0.7)
+//
+//        label.setUpdateBlock { value, label in
+//            label.text = String(format: "%.2f%", value)
+//        }
+//    }
 }
-
